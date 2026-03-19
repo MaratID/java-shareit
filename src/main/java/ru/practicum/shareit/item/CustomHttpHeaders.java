f87122422500e269de5c213a7272a3ecefe3cf4e
MaratID <vasuu-1982@yandex.ru>
2026-03-19 19:33:13 +0300

    ver_8

--- src/main/java/ru/practicum/shareit/item/CustomHttpHeaders.java
+++ src/main/java/ru/practicum/shareit/item/CustomHttpHeaders.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpHeaders;
 public class CustomHttpHeaders extends HttpHeaders {
     public static final String userId = "X-Sharer-User-Id";
 
-    public CustomHttpHeaders() {}
+    public CustomHttpHeaders() {
+        
+    }
 
 }
